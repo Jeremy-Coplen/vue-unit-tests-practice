6dff2d77a00acfb1c94da5c10bbd8aa7f9a311eb
Jeremy-Coplen <jeremycoplen.jc@gmail.com>
2022-05-03 04:49:43 -0600

    First commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+  </header>
+
+  <RouterView />
+</template>
+
+<style>
+@import '@/assets/base.css';
+
+#app {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+}
+</style>
